6f9837a8b0cd67a755648b2e561bc0684cc11230
Pankaj Kumar <pankaj.kumar.phe21@itbhu.ac.in>
2025-07-18 19:38:43 +0530

    vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,11 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.get('/',(req,res)=>{
+  const date=new Date(Date.now());
+  res.send('server is running');
+})
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
