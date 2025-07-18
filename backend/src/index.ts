b184bed50f6298da315b75b027589053edd11619
Pankaj Kumar <pankaj.kumar.phe21@itbhu.ac.in>
2025-07-18 19:59:50 +0530

    vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,7 @@ app.use(express.json());
 
 app.get('/',(req,res)=>{
   const date=new Date(Date.now()).toLocaleString('en-IN',{hour12:false});
+  console.log(date);
   res.send(JSON.stringify([date]));
 })
 
