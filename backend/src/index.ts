1e410567648049a226ecc4660dea155e201c7278
Pankaj Kumar <pankaj.kumar.phe21@itbhu.ac.in>
2025-07-19 20:45:18 +0530

    vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,26 +13,24 @@ const indiaTime = date.tz('Asia/Kolkata').format('YYYY-MM-DD HH:mm:ss');
 const usEasternTime = date.tz('America/New_York').format('YYYY-MM-DD HH:mm:ss');
 const londonTime = date.tz('Europe/London').format('YYYY-MM-DD HH:mm:ss');
 
-console.log('India Time:', indiaTime);
-console.log('US Eastern Time:', usEasternTime);
-console.log('London Time:', londonTime);
-
   res.send(JSON.stringify([indiaTime,usEasternTime,londonTime,new Date(Date.now()).toUTCString(),new Date(Date.now()).toISOString()]));
 })
 
 app.get('/test',(req,res)=>{
-  const timestamp=JSON.stringify(new Date(Date.now()).toISOString());
+  const timestamp=new Date(Date.now()).toISOString();
+  console.log(timestamp)
   if (!timestamp) res.send(0);
   try {
     const istTime = new Date(timestamp).toLocaleString('en-US', { hour12: false });
+    console.log(istTime);
     const [, timePart] = istTime.split(', ');
     const [hourStr] = timePart.split(':');
     const hour = parseInt(hourStr, 10);
     res.send( JSON.stringify([hour >= 9 && hour < 18 ? 1 : 2,timestamp,istTime]));
   } catch {
     console.warn('Failed to determine shift_id from timestamp');
+    res.send('Hi');
   }
-  res.send('Hi');
 })
 
 app.listen(PORT, () => {
