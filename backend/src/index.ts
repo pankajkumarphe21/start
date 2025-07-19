17c0d842be924f6e2340328618ecd7576039ed2d
Pankaj Kumar <pankaj.kumar.phe21@itbhu.ac.in>
2025-07-19 20:40:31 +0530

    vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,6 +20,21 @@ console.log('London Time:', londonTime);
   res.send(JSON.stringify([indiaTime,usEasternTime,londonTime,new Date(Date.now()).toUTCString(),new Date(Date.now()).toISOString()]));
 })
 
+app.get('/test',(req,res)=>{
+  const timestamp=JSON.stringify(new Date(Date.now()).toISOString());
+  if (!timestamp) res.send(0);
+  try {
+    const istTime = new Date(timestamp).toLocaleString('en-US', { hour12: false });
+    const [, timePart] = istTime.split(', ');
+    const [hourStr] = timePart.split(':');
+    const hour = parseInt(hourStr, 10);
+    res.send( JSON.stringify([hour >= 9 && hour < 18 ? 1 : 2,timestamp,istTime]));
+  } catch {
+    console.warn('Failed to determine shift_id from timestamp');
+  }
+  res.send('Hi');
+})
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
