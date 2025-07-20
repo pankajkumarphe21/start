cae885d25715ba8cf0c176b8589b881c7b92cb96
Pankaj Kumar <pankaj.kumar.phe21@itbhu.ac.in>
2025-07-20 13:37:57 +0530

    updation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,8 +17,10 @@ const londonTime = date.tz('Europe/London').format('YYYY-MM-DD HH:mm:ss');
 })
 
 app.get('/test',(req,res)=>{
-  const timestamp=new Date(Date.now()).toISOString();
-  console.log(timestamp)
+  let timestamp=new Date().toISOString();
+  const currentTime = new Date().toISOString();
+  const date = new Date(currentTime).toISOString().split('T')[0];
+  console.log(new Date().toString());
   if (!timestamp) res.send(0);
   try {
     const istTime = new Date(timestamp).toLocaleString('en-US', { hour12: false });
@@ -26,7 +28,7 @@ app.get('/test',(req,res)=>{
     const [, timePart] = istTime.split(', ');
     const [hourStr] = timePart.split(':');
     const hour = parseInt(hourStr, 10);
-    res.send( JSON.stringify([hour >= 9 && hour < 18 ? 1 : 2,timestamp,istTime]));
+    res.send( JSON.stringify([hour >= 9 && hour < 18 ? 1 : 2,timestamp,istTime,date]));
   } catch {
     console.warn('Failed to determine shift_id from timestamp');
     res.send('Hi');
